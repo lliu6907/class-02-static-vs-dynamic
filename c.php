5610b0d3228684697ab13afd8945df857e8e679c
Lina Liu <82733958+lliu6907@users.noreply.github.com>
2024-01-24 14:05:34 +0000

    Replaced TODOs

--- c.php
+++ c.php
@@ -9,7 +9,7 @@
   <h1>Page C</h1>
   <p>Is this page static, client-side rendered, or server-side rendered? (Replace TODO with your answer.)</p>
   <p><?php echo htmlspecialchars(date("l jS \o\f F Y h:i:s A")); ?></p>
-  <h2>Answer: TODO</h2>
+  <h2>Answer: This is a dynamic webpage and it is client-side rendered because rendering happens in the DOM rather than in the server.</h2>
 </body>
 
 </html>
