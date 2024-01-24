e9c4f18586b5647dca77beccb8a01edcacad60d7
Lina Liu <82733958+lliu6907@users.noreply.github.com>
2024-01-24 14:14:06 +0000

    Revised answer

--- c.php
+++ c.php
@@ -9,7 +9,7 @@
   <h1>Page C</h1>
   <p>Is this page static, client-side rendered, or server-side rendered? (Replace TODO with your answer.)</p>
   <p><?php echo htmlspecialchars(date("l jS \o\f F Y h:i:s A")); ?></p>
-  <h2>Answer: This is a dynamic webpage and it is client-side rendered because rendering happens in the DOM rather than in the server.</h2>
+  <h2>Answer: This is a dynamic webpage and it is server side rendering because there is no use of Javascript. Moreover, the HTML code is complete with the content being there.</h2>
 </body>
 
 </html>
