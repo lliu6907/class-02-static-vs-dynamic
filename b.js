8411a66acc97f6b2024885b1859da1faf9260c60
Lina Liu <82733958+lliu6907@users.noreply.github.com>
2024-01-24 14:16:41 +0000

    Finished TODO B

--- b.js
+++ b.js
@@ -1,6 +1,6 @@
 const h1 = React.createElement('h1', null, 'Page B');
 const p = React.createElement('p', null, 'Is this page static, client-side rendered, or server-side rendered? (Replace TODO with your answer.)');
 const p_date = React.createElement('p', null, new Date().toString());
-const h2 = React.createElement('h2', null, 'Answer: TODO');
+const h2 = React.createElement('h2', null, 'Answer: This is client-side rendered because it uses JS and the HTML is not complete since some of the content is dynamically created in the JS');
 
 ReactDOM.render([h1, p, p_date, h2], document.body);
